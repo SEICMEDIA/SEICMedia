5c1388f3a676622be79f17c72c703ff20a2dffd1
Michael Ambrose <ma8540@rit.edu>
2023-07-20 20:02:07 -0400

    init2

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-// Scroll to section on navigation link click
 $(document).ready(function() {
-    $("nav a").on('click', function(event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-          window.location.hash = hash;
-        });
-      }
-    });
-  });  
+    // Fade in the title
+    $('header h1').addClass('animated fadeIn');
+  
+    // Generate stars and animate them
+    const totalStars = 100;
+    for (let i = 0; i < totalStars; i++) {
+      createStar();
+    }
+  });
+  
+  function createStar() {
+    const star = document.createElement('div');
+    star.classList.add('star');
+    star.style.top = `${randomPosition()}px`;
+    star.style.left = `${randomPosition()}px`;
+    star.style.animationDelay = `${randomDelay()}s`;
+    star.style.animationDuration = `${randomDuration()}s`;
+    star.style.animationIterationCount = 'infinite';
+    document.querySelector('.stars').appendChild(star);
+  }
+  
+  function randomPosition() {
+    return Math.floor(Math.random() * window.innerWidth);
+  }
+  
+  function randomDelay() {
+    return Math.random() * 5;
+  }
+  
+  function randomDuration() {
+    return Math.random() * 3 + 1;
+  }
+  
